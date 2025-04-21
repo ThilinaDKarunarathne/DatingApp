5eced06e58cc5e0f8d656c3956640337e844350f
Thilina Dhanajaya <adamsampath2002@gmail.com>
2025-04-21 16:23:40 +0530

    ErrorHandling

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70777a1aebc194951aefd6954ae34c53e63bc041")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11b6c15b7609a1783fcf2f422781e2e3d0d767c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
