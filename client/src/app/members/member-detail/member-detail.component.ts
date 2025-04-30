360acf90241a881bd6bf41b34f64c7c25fb559f5
Thilina Dhanajaya <adamsampath2002@gmail.com>
2025-04-30 17:35:55 +0530

    merge branches

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -3,20 +3,13 @@ import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-<<<<<<< HEAD
 import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
-=======
->>>>>>> 9f6d4a6549240ebac489048959e6e8c46f92b5fc
 
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-<<<<<<< HEAD
   imports: [TabsModule, GalleryModule] ,
-=======
-  imports: [TabsModule] ,
->>>>>>> 9f6d4a6549240ebac489048959e6e8c46f92b5fc
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
@@ -24,11 +17,8 @@ export class MemberDetailComponent {
   private memberSetvice = inject(MembersService);
   private route = inject(ActivatedRoute);
   member?: Member;
-<<<<<<< HEAD
   images: GalleryItem[] = [];
 
-=======
->>>>>>> 9f6d4a6549240ebac489048959e6e8c46f92b5fc
   
 
   ngOnInit(): void {
@@ -39,7 +29,6 @@ export class MemberDetailComponent {
     const username = this.route.snapshot.paramMap.get('username');
     if(!username) return;
     this.memberSetvice.getMember(username).subscribe({
-<<<<<<< HEAD
       next: member => {
         this.member = member;
         member.photos.map(p => {
@@ -48,10 +37,5 @@ export class MemberDetailComponent {
     }
   })
  }
-=======
-      next: member => this.member = member
-    })
-  }
->>>>>>> 9f6d4a6549240ebac489048959e6e8c46f92b5fc
 
 }
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -5,18 +5,11 @@ import { Member } from '../../_models/member';
 import { MemberCardComponent } from "../member-card/member-card.component";
 
 @Component({
-<<<<<<< HEAD
-    selector: 'app-member-list',
-    imports: [MemberCardComponent],
-    templateUrl: './member-list.component.html',
-    styleUrl: './member-list.component.css'
-=======
   selector: 'app-member-list',
   standalone: true,
   imports: [MemberCardComponent],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
->>>>>>> 9f6d4a6549240ebac489048959e6e8c46f92b5fc
 })
 export class MemberListComponent implements OnInit{
   private memberService = inject(MembersService);
